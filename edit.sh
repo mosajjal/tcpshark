ee11167685e26bd1a70b8ab0bede9ad2e823f567
Alireza Mosajjal <mosajjal@gmail.com>
2021-03-17 18:37:30 +1300

    edit.sh now has a JSON comment for each packet

--- edit.sh
+++ edit.sh
@@ -9,7 +9,7 @@ do
     tempfile="tmp_`echo $frame`.pcapng"
     echo "Processing packet in frame $frame to $tempfile"
     tshark -r $file -w $tempfile -Y "frame.number==$frame"
-    editcap -a 1:"New TCP SYN" $tempfile Commented_$tempfile
+    editcap -a 1:'{"interface":"lo", pid:"1", program:"/bin/ping", arguments:"1.1.1.1 -t"}' $tempfile Commented_$tempfile
 done
 exclude=`cat exclusion`
 editcap $file excluded_$file $exclude
@@ -17,5 +17,5 @@ mergecap -w Commented_$file Commented_tmp_*.pcapng excluded_$file
 
 rm tmp_*.pcapng
 rm Commented_tmp_*.pcapng
-rm excluded _*.pcapng
+rm excluded_*.pcapng
 rm exclusion

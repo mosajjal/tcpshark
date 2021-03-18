e7692371b479b782f78a54d651ee77cc6b80db68
Alireza Mosajjal <mosajjal@gmail.com>
2021-03-18 18:37:52 +1300

    minor changes to the way packet capture starts

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"os/signal"
+	"runtime"
 	"time"
 
 	"github.com/google/gopacket"
+
 	"github.com/google/gopacket/pcap"
 	"github.com/google/gopacket/pcapgo"
 )
@@ -18,16 +19,34 @@ func errorHandler(err error) {
 	}
 }
 
+var myNgInterface = pcapgo.NgInterface{
+	Name:                "lo",
+	OS:                  runtime.GOOS,
+	SnapLength:          0, //unlimited
+	TimestampResolution: 9,
+}
+
+var myNgWriterOptions = pcapgo.NgWriterOptions{
+	SectionInfo: pcapgo.NgSectionInfo{
+		Hardware:    runtime.GOARCH,
+		OS:          runtime.GOOS,
+		Application: "gopacket", //spread the word
+	},
+}
 var outputChannel = make(chan gopacket.Packet, 100)
+var done = make(chan bool)
 
 func main() {
-	go printPacket(outputChannel)
+	w := initializeOutputPcap("test.pcapng")
+
+	go printPacket(outputChannel, w)
 	// pid, process name, process full path, time, start, user
 	// fmt.Println(process.Processes())
 	// pid, source address, source port, dest address, dest port, status
 	// fmt.Println(net.Interfaces())
 	// start capture
-	start("lo", "icmp")
+	handleInterrupt(done)
+	start("lo", "")
 	// correlate source and dest socket from each packet to the network, find the pid and add context of the pid to the packet "data"
 	// ???
 	// push this somehow to pcap-ng or sth
@@ -35,8 +54,13 @@ func main() {
 }
 
 func initializeOutputPcap(filePath string) *pcapgo.NgWriter {
-	f, _ := os.OpenFile(filePath, os.O_APPEND, 0700)
-	w, _ := pcapgo.NewNgWriter(f, 1)
+	f, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE, 0755)
+	errorHandler(err)
+	// defer f.Close()
+	w, err := pcapgo.NewNgWriter(f, 1)
+	errorHandler(err)
+	_, err = w.AddInterface(myNgInterface)
+	errorHandler(err)
 	return w
 }
 
@@ -66,14 +90,15 @@ func handleInterrupt(done chan bool) {
 	}()
 }
 
-func printPacket(packet chan gopacket.Packet) {
-	writer := initializeOutputPcap("./test.pcapng")
+func printPacket(packet chan gopacket.Packet, w *pcapgo.NgWriter) {
+
 	for {
 		select {
 		case p := <-packet:
-			fmt.Println("here")
-			writer.WritePacket(p.Metadata().CaptureInfo, p.Data())
-			writer.Flush()
+			err := w.WritePacket(p.Metadata().CaptureInfo, p.Data())
+			//TODO: this write should be once every second
+			w.Flush()
+			errorHandler(err)
 		}
 
 	}
@@ -83,8 +108,8 @@ func start(DevName string, pbf string) {
 	handle := initializeLivePcap(DevName, pbf)
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 
-	packetSource.DecodeOptions.Lazy = true
-	packetSource.NoCopy = true
+	// packetSource.DecodeOptions.Lazy = true
+	// packetSource.NoCopy = true
 
 	for {
 		select {
@@ -92,10 +117,12 @@ func start(DevName string, pbf string) {
 			if packet == nil {
 				log.Println("PacketSource returned nil, exiting (Possible end of pcap file?). Sleeping for 10 seconds waiting for processing to finish")
 				time.Sleep(time.Second * 10)
-				// close(options.Done)
+				close(done)
 				return
 			}
 			outputChannel <- packet
+		case <-done:
+			return
 
 		}
 
